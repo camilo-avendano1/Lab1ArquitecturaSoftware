d3abb63c10b6650892e11f13d86018199efd268f
Jose Waldo <josferw@gmail.com>
2023-09-16 18:11:39 -0500

    Añadi el controller del captcha y también las vistas

---
+++ Lab1ArquitecturaSoftware-web/src/main/java/com/udea/controller/Captcha.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.udea.controller;
+
+/**
+ *
+ * @author jose_waldo
+ */
+public class Captcha {
+    public Captcha() {
+        
+    }
+}
